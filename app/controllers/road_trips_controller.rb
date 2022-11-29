d60da2911c66573999b39bd826de4b37c192b68a
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-11-29 14:40:44 +0100

    controller

--- app/controllers/road_trips_controller.rb
+++ app/controllers/road_trips_controller.rb
@@ -1,11 +1,19 @@
 class RoadTripsController < ApplicationController
   def index
+    if params[:query].present?
+      sql_query = "country ILIKE :query OR town ILIKE :query"
+      @road_trips = RoadTrip.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @road_trips = RoadTrip.all
+    end
   end
 
   def show
+    @road_trip = RoadTrip.find(params[:id])
   end
 
   def new
+    @road_trip = RoadTrip.new()
   end
 
   def create
