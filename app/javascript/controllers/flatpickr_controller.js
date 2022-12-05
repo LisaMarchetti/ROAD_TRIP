4bb947efd5dbc0438dc80b9321da2eaec1cdb487
Lisa Marchetti <lisa3marchetti@gmail.com>
2022-12-05 20:36:01 +0100

    debut front create avec Maewenn

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -3,14 +3,6 @@
 import { Controller } from "@hotwired/stimulus";
 import flatpickr from "flatpickr";
 
-// export default class extends Controller {
-//   connect() {
-//     new flatpickr(this.element, {
-//       enableTime: true
-//       // more options available on the documentation!
-//     });
-//   }
-// }
 
 export default class extends Controller {
   // Inform the controller that it has two targets in the form, which are our inputs
