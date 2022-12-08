c82fe2261035f59a9680d224289caddf323366aa
Gohar Vardanyan <gohsamvelyan@gmail.com>
2022-12-08 17:25:44 +0100

    meta titles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,4 +27,8 @@ class ApplicationController < ActionController::Base
       @my_participations = current_user.participations
     end
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
 end
